00fb9d5f411a91fe0d1ad5a5cb9c4bd94c25ebeb
Jailce Fernanda <n03suokrwzi1jlaoh41x79be2gavcc@bots.bitbucket.org>
2024-09-29 23:27:09 -0300

    add export no json

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     images: {
       domains: ['cdn.pixabay.com'],
+      output: 'export', // Adiciona exportação estática
+      basePath: '/landingpage_enem', // Substitua "reponame" pelo nome do seu repositório
+      trailingSlash: true, // Adiciona uma barra ao final das URLs
     },
   };
   
