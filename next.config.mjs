d0cc0e29c8f0de66100af380c7fbabd3079be83a
Jailce Fernanda <n03suokrwzi1jlaoh41x79be2gavcc@bots.bitbucket.org>
2024-10-03 21:31:49 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
   images: {
     domains: ['cdn.pixabay.com'],
   },
-  // Adicione esta linha para permitir a exportação estática
   trailingSlash: true, // Isso ajuda com a compatibilidade de rotas
   // Configure o caminho base para o GitHub Pages
   assetPrefix: process.env.NODE_ENV === 'production' ? '/landingpage_enem/' : '',
+  output: 'export', // Adicione esta linha para permitir a exportação estática
 };
 
 export default nextConfig;
