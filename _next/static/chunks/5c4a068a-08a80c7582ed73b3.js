549bd44b2e5f7c275b0df5f06768da4ded5613b3
Jailce Fernanda <n03suokrwzi1jlaoh41x79be2gavcc@bots.bitbucket.org>
2024-10-03 22:49:36 -0300

    Updates

---
+++ _next/static/chunks/5c4a068a-08a80c7582ed73b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[555],{9690:function(a,e,n){n.d(e,{s$V:function(){return u}});let u=(0,n(852).k)("PhoneRegular","1em",["M9 14a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H9ZM7 2a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h6a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7ZM6 4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4Z"])}}]);
